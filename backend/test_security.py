7bd26bb41d489d7274d50e76511cbce4dcd89a37
Rohan <rohantiwari166@gmail.com>
2026-07-29 18:36:07 +0530

    chore: remove temporary security test

--- backend/test_security.py
+++
@@ -1,9 +0,0 @@
-from app.core.security import hash_password, verify_password
-
-password = "Rohan123"
-
-hashed = hash_password(password)
-
-print(hashed)
-print(verify_password(password, hashed))
-print(verify_password("WrongPassword", hashed))
